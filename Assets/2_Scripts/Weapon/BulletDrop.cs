17792adb08bbd87ee1ce7e80134221a0df3a46b9
Zayniddin Kobilov <102047747+Zidan-kohai@users.noreply.github.com>
2024-06-29 05:06:04 +0300

    try to add weapon dropdawn system

---
+++ Assets/2_Scripts/Weapon/BulletDrop.cs
@@ -0,0 +1,15 @@
+﻿using Base.Data;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class BulletDrop : MonoBehaviour, IPointerClickHandler
+{
+    [SerializeField] private int id;
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        Data.Instance.OpenWeapon(id);
+        Debug.Log("Click on Drop weapon with id:" + id);
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/2_Scripts/Weapon/DropDawnWeapon.cs
@@ -0,0 +1,57 @@
+using Base.Data;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DropDawnWeapon : MonoBehaviour
+{
+    [SerializeField] private BulletDrop gun_00;
+    [SerializeField] private BulletDrop gun_01;
+    [SerializeField] private BulletDrop gun_02;
+    [SerializeField] private BulletDrop gun_03;
+
+    [SerializeField] private int Ypos = 10, maxXpos = -5, minXpos = 5;
+
+
+    public int gun_00Chance = 100;
+    public int gun_01Chance = 200;
+    public int gun_02Chance = 300;
+    public int gun_03Chance = 400;
+
+
+
+    public void Update()
+    {
+        if (!Data.Instance.SaveData.openWeaponId.Contains(0) && CheckDropDawn(gun_00Chance))
+        {
+            SpawnDropDawnGun(gun_00);
+        }
+
+        if (!Data.Instance.SaveData.openWeaponId.Contains(1) && CheckDropDawn(gun_01Chance))
+        {
+            SpawnDropDawnGun(gun_01);
+        }
+
+        if (!Data.Instance.SaveData.openWeaponId.Contains(2) && CheckDropDawn(gun_02Chance))
+        {
+            SpawnDropDawnGun(gun_02);
+        }
+
+        if (!Data.Instance.SaveData.openWeaponId.Contains(3) && CheckDropDawn(gun_03Chance))
+        {
+            SpawnDropDawnGun(gun_03);
+        }
+    }
+
+    private bool CheckDropDawn(int chance)
+    {
+        return UnityEngine.Random.Range(0, chance) < 1;
+    }
+
+    private void SpawnDropDawnGun(BulletDrop gun_00)
+    {
+        BulletDrop drop = Instantiate(gun_00);
+
+        drop.transform.position = new Vector3(UnityEngine.Random.Range(minXpos, maxXpos), Ypos, 0);
+    }
+}
