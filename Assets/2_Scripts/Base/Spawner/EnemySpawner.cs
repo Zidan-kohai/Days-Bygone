f91d43564e8891999b21ddbb223fb09dcfa12301
Zayniddin Kobilov <102047747+Zidan-kohai@users.noreply.github.com>
2024-07-07 18:20:03 +0300

    fix boss position

--- Assets/2_Scripts/Base/Spawner/EnemySpawner.cs
+++ Assets/2_Scripts/Base/Spawner/EnemySpawner.cs
@@ -51,34 +51,34 @@ namespace Base.Spawner
                     switch (enemy.Type)
                     {
                         case EnemyType.Skeleton:
-                            Init(enemyPool.GetFree<Skeleton>());
+                            Init(enemyPool.GetFree<Skeleton>(), minStartPos, maxStartPos);
                             break;
                         case EnemyType.Halk:
-                            Init(enemyPool.GetFree<Halk>());
+                            Init(enemyPool.GetFree<Halk>(), minStartPos, maxStartPos);
                             break;
                         case EnemyType.Hammerman:
-                            Init(enemyPool.GetFree<Hammerman>());
+                            Init(enemyPool.GetFree<Hammerman>(), minStartPos, maxStartPos);
                             break;
                         case EnemyType.Cyborg:
-                            Init(enemyPool.GetFree<Cyborg>());
+                            Init(enemyPool.GetFree<Cyborg>(), minStartPos, maxStartPos);
                             break;
                         case EnemyType.Goblin:
-                            Init(enemyPool.GetFree<Goblin>());
+                            Init(enemyPool.GetFree<Goblin>(), minStartPos, maxStartPos);
                             break;
                         case EnemyType.MushroomMan:
-                            Init(enemyPool.GetFree<MushroomMan>());
+                            Init(enemyPool.GetFree<MushroomMan>(), minStartPos, maxStartPos);
                             break;
                         case EnemyType.Neanderthal:
-                            Init(enemyPool.GetFree<Neanderthal>());
+                            Init(enemyPool.GetFree<Neanderthal>(), minStartPos, maxStartPos);
                             break;
                         case EnemyType.Neanderthal2:
-                            Init(enemyPool.GetFree<Neanderthal2>());
+                            Init(enemyPool.GetFree<Neanderthal2>(), minStartPos, maxStartPos);
                             break;
                         case EnemyType.RedfGoblin:
-                            Init(enemyPool.GetFree<RedfGoblin>());
+                            Init(enemyPool.GetFree<RedfGoblin>(), minStartPos, maxStartPos);
                             break;
                         case EnemyType.Slime:
-                            Init(enemyPool.GetFree<Slime>());
+                            Init(enemyPool.GetFree<Slime>(), minStartPos, maxStartPos);
                             break;
                         case EnemyType.BigGoblin:
                             if(enemies.Count > 0) 
@@ -87,7 +87,7 @@ namespace Base.Spawner
                                 i--;
                                 continue;
                             }
-                            Init(enemyPool.GetFree<BigGoblin>());
+                            Init(enemyPool.GetFree<BigGoblin>(), minStartPos, 2.5f);
                             break;
                         case EnemyType.Demon:
                             if (enemies.Count > 0)
@@ -96,7 +96,7 @@ namespace Base.Spawner
                                 i--;
                                 continue;
                             }
-                            Init(enemyPool.GetFree<Demon>());
+                            Init(enemyPool.GetFree<Demon>(), minStartPos, 2.5f);
                             break;
                         case EnemyType.Serber:
                             if (enemies.Count > 0)
@@ -105,7 +105,7 @@ namespace Base.Spawner
                                 i--;
                                 continue;
                             }
-                            Init(enemyPool.GetFree<Serber>());
+                            Init(enemyPool.GetFree<Serber>(), minStartPos, 2.5f);
                             break;
                         case EnemyType.Worm:
                             if (enemies.Count > 0)
@@ -114,7 +114,7 @@ namespace Base.Spawner
                                 i--;
                                 continue;
                             }
-                            Init(enemyPool.GetFree<Worm>());
+                            Init(enemyPool.GetFree<Worm>(), minStartPos, 2.5f);
                             break;
                         case EnemyType.Santa:
                             if (enemies.Count > 0)
@@ -123,7 +123,7 @@ namespace Base.Spawner
                                 i--;
                                 continue;
                             }
-                            Init(enemyPool.GetFree<Santa>());
+                            Init(enemyPool.GetFree<Santa>(), minStartPos, 2.5f);
                             break;
                     }
                 }
@@ -132,11 +132,14 @@ namespace Base.Spawner
             this.isLastWave = isLastWave;
         }
 
-        private void Init<T>(T instance) where T : Enemy
+        private void Init<T>(T instance, float minStartPos, float maxStartPos) where T : Enemy
         {
             enemies.Add(instance);
+
             float randomYPos = Random.Range(minStartPos, maxStartPos);
+
             instance.transform.position = new Vector3(10, randomYPos, 0);
+
             instance.Init(CheckWin);
         }
 
